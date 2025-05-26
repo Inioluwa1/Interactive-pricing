ed5820584e7cfb63d5abfec5a387ca3254875342
Inioluwa1 <adesanyanifesimi@gmail.com>
2025-05-26 10:30:36 +0100

    Project Started

---
+++ Pages/Options.jsx
@@ -0,0 +1,29 @@
+const Options = [
+  {
+    id: 1,
+    title: '10K',
+    price: '8',
+  },
+  {
+    id: 2,
+    title: '50K',
+    price: '12',
+  },
+  {
+    id: 3,
+    title: '100K',
+    price: '16',
+  },
+  {
+    id: 4,
+    title: '500K',
+    price: '24',
+  },
+  {
+    id: 5,
+    title: '1M',
+    price: '36',
+  },
+]
+
+export default Options;
